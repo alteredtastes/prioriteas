43f5e241d765fb640134bc7cf55d39680505fadb
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2016-05-23 19:33:00 -0700

    get teas and get 2 versions of get categories

--- services/inventory-service.js
+++ services/inventory-service.js
@@ -7,18 +7,45 @@
 
   function InventoryService($http){
 
-    var teas = $http.get('teas.json').then(function(result){
-      return result.data;
-    });
+    var temp = [];
+    var categories = [];
+
+    function Teas() {
+      return $http.get('teas.json').then(function(result){
+        return result.data.teas;
+      })
+    }
 
     return {
       getTeas: function(){
-        return teas;
+        return Teas();
       },
-      getByCategory: function(category) {
-        
+      // getCategories: function(){
+      //   return Teas().then(function(teas) {
+      //     for (var i = 0; i < teas.length; i++) {
+      //       for (var j = 0; j < teas[i].categories.length; j++) {
+      //         if(categories.indexOf(teas[i].categories[j]) === -1) {
+      //           categories.push(teas[i].categories[j])
+      //         }
+      //       }
+      //     }
+      //     return categories;
+      //   })
+      // },
+      getCategories: function() {
+        return Teas().then(function(teas) {
+          teas.forEach(function(obj) {
+            temp.push(obj.categories);
+          })
+        temp = [].concat.apply([], temp);
+        for (var i = 0; i < temp.length; i++) {
+          if(categories.indexOf(temp[i]) === -1) {
+            categories.push(temp[i]);
+            }
+          }
+        return categories;
+        })
       }
-
     }
-  }
+}
 })();
--- store/store-controller.js
+++ store/store-controller.js
@@ -10,7 +10,16 @@
     vm.storeView = 'this is the store';
 
     InventoryService.getTeas().then(function(data){
-      vm.teas = data.teas;
+      vm.teas = data;
+      console.log(vm.teas);
     })
+
+    InventoryService.getCategories().then(function(data){
+      vm.categories = data;
+      console.log(vm.categories);
+    })
+
+    InventoryService.getCategoryBy
+
   }
 })();
